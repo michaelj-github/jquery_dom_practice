a417a5df6c15ebf9740a92d01db7e68dec936954
michaelj-github <mike@michaeljmurphy.com>
2022-04-07 22:19:34 -0400

    update js file for rating min and max check

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $("#aMovie").on("click", function(e) {
   e.preventDefault();
   let theMovieTitle = $("#movieTitle").val();
   let theMovieRating = $("#movieRating").val()
-  if (theMovieTitle.length >= 2 && theMovieRating != '') {
+  if (theMovieTitle.length >= 2 && theMovieRating != '' && theMovieRating >= 0 && theMovieRating <= 10) {
     if ($("#theTable td").length === 0) {
       $("#theTable").removeClass("tableViewOff").addClass("tableViewOn");
     }
@@ -16,5 +16,9 @@ $("#aMovie").on("click", function(e) {
     $("#theTable").append("<tr><td class=tdTitle>" + theMovieTitle + "</td><td class=tdRating>" + theMovieRating + "</td><td id=" + `R${theRow}` + " class=deleteColumn>" + "X" + "</td></tr>");
     $("#movieTitle").val('');
     $("#movieRating").val('');
+  } else {
+    if (theMovieRating <= 0 || theMovieRating >= 10) {
+      $("#movieRating").val('');
+    };
   }
 });
